9daafe09694698e355cbb8961641e8525f0c8c25
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 15:29:08 -0300

    Comecando a implementar o tabuleiro

---
+++ Aula-161/Program.cs
@@ -0,0 +1,14 @@
+﻿using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            
+        }
+    }
+}
