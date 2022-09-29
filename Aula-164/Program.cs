3d214ccbdd0027a53de2a8d1f799e938d3dd43b6
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 17:01:48 -0300

    Criando uma excecao personalizada de tabuleiro

---
+++ Aula-164/Program.cs
@@ -0,0 +1,21 @@
+﻿using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+
+            Tela.ImprimirTabuleiro(tab);
+
+
+        }
+    }
+}
