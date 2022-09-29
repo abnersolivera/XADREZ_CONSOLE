f44d124c40ab993806c70cdb9b8dedf0d79e468a
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 17:12:26 -0300

    Tratando erros com excecao personalizada!

---
+++ Aula-165/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.ConstrainedExecution;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
