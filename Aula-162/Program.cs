501075b7752558b2249ba044fc25339ff3d9215d
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 15:50:46 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

---
+++ Aula-162/Program.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.ImprimirTabuleiro(tab);
+
+
+        }
+    }
+}
