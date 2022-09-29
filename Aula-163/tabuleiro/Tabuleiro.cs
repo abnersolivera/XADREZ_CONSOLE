03ceb8af730429fcac134e3ec5145fee20c4fc60
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 16:44:54 -0300

    Colocando pecas no tabuleiro

---
+++ Aula-163/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,27 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[Linhas, Colunas];
+        }
+
+        public Peca Peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
+    }
+}
