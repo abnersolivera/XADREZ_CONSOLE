10b80839fe8b76e91b419d17e1b26c538e2a6c4d
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-09-29 17:53:03 -0300

    Criando a posicao xadrez

---
+++ Aula-166/Program.cs
@@ -0,0 +1,17 @@
+﻿using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoXadrez pos = new PosicaoXadrez('c',1);
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosicao());
+        }
+    }
+}
---
+++ Aula-166/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca (Cor cor, Tabuleiro tab)
+        {
+            Posicao = null;
+            Cor = cor;
+            Tab = tab;
+            QteMovimentos = 0;
+        }
+    }
+}
